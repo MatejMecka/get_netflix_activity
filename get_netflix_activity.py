001b8768dc5c93878b9fa19b1e5155a9c0c0087d
ManveerBasra <m13basra@gmail.com>
2016-12-02 20:43:46 -0800

    Minor bug fixes and major improvements

--- get_netflix_activity.py
+++ get_netflix_activity.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from configparser import ConfigParser
+import time
 
 # Variable that dictates whether output is displayed on the console
 show_output = True
@@ -63,13 +64,20 @@ class NetflixRetreiver(object):
             driver.find_element_by_class_name('login-button').click()
             mutliPageLogin = True
 
-        driver.implicitly_wait(1)
+        time.sleep(1)
 
         if mutliPageLogin:
             driver.find_element_by_name('password').clear()
 
         # Typing in user's password
         driver.find_element_by_name('password').send_keys(self.password)
+
+        # Sometimes Netflix displays the password in a drop-down menu, if that happens
+        # this block of code hides the password
+        try:
+            driver.find_elements_by_link_text('Hide Password').click()
+        except:
+            pass
 
         # Clicking on submit button
         driver.find_element_by_class_name('login-button').click()
@@ -92,44 +100,81 @@ class NetflixRetreiver(object):
                     if profile_names[i].text == self.profile_name:
                         # Click profile image associated with name
                         profiles[i].click()
+                    elif i == len(profiles):
+                        print('Error: Your profile name (\'%s\') was not found.' % self.profile_name)
+                        print('       Please check if you entered the correct profile name in \'userconfig.ini\'')
+                        driver.close()
                 except StaleElementReferenceException:
                     pass
 
         # Using the commented block of code below didn't work on some systems
         # So instead a new tab is opened for the 'viewing activity' page
-        driver.implicitly_wait(3)
+        time.sleep(1)
         # driver.get('https://www.netflix.com/viewingactivity')
-        driver.execute_script('window.open(\'https://www.netflix.com/viewingactivity\',\'_blank\');')
-        driver.switch_to.window(driver.window_handles[-1])
+        # driver.execute_script('window.open(\'https://www.netflix.com/viewingactivity\',\'_blank\');')
+        # driver.switch_to.window(driver.window_handles[-1])
 
         # Wait for browse page to load
-        # wait = WebDriverWait(driver, 30)
-        # wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'profile-name')))
-        # if show_output:
-        #    print('Navigating Site')
+        WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.CLASS_NAME, 'profile-icon')))
+        if show_output:
+            print('Navigating Site')
 
-        # profile_icon = driver.find_element_by_class_name('profile-name')
         # your_account = driver.find_element_by_link_text('Your Account')
         # hov = ActionChains(driver).move_to_element(profile_icon).move_to_element(your_account).click()
         # hov.perform()
 
-        # hov = ActionChains(driver).move_to_element(profile_icon)
-        # hov.perform()
-
         # Hover then click sometimes fails, so it runs until it works with a maximum of 3 attempts
-        # for i in range(3):
-        #    try:
-        #        driver.find_element_by_link_text('Your Account').click()
-        #        break
-        #    except:
-        #        profile_icon = driver.find_element_by_class_name('profile-name')
-        #        hov = ActionChains(driver).move_to_element(profile_icon)
-        #        hov.perform()
+        hov_profile = driver.find_element_by_class_name('profile-icon')
+        error_count = 0
+        while True:
+            try:
+                # On some systems the profile icon is displayed differently.
+                # For this reason if the previous attempt doesn't work,
+                # the program searches for a different item that will do the same thing when clicked as the profile-icon
+                if error_count == 1:
+                    hov_profile = driver.find_element_by_class_name('profile-name')
+                elif error_count == 2:
+                    hov_profile = driver.find_element_by_class_name('profile-arrow')
+                elif error_count == 3:
+                    hov_profile = driver.find_element_by_class_name('avatar')
+                elif error_count == 4:
+                    hov_profile = driver.find_element_by_class_name('profile-link')
+                elif error_count == 5:
+                    hov_profile = driver.find_element_by_class_name('account-dropdown-button')
+                elif error_count == 6:
+                    hov_profile = driver.find_element_by_class_name('account-menu-item')
+                elif error_count == 7:
+                    hov_profile = driver.find_element_by_class_name('current-profile')
+                else:
+                    print('Error: Program was unable to find profile picture. Please report this issue')
+                    break
 
-        # Navigate to page containing viewing activity
-        # driver.find_element_by_link_text('Viewing activity').click()
+                # Attempt to move to profile icon
+                hov = ActionChains(driver).move_to_element(hov_profile)
+                hov.perform()
 
-        driver.implicitly_wait(2)
+                # Click on 'Your Account' which appears from drop-down menu provoked in previous step
+                driver.find_element_by_link_text('Your Account').click()
+
+                # Navigate to page containing viewing activity
+                driver.find_element_by_link_text('Viewing activity').click()
+                break
+            except:
+                error_count += 1
+
+        time.sleep(2)
+        # Scroll to the bottom of the page
+        original = len(driver.find_elements_by_class_name('retableRow'))
+        while True:
+            print('running loop')
+            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+            time.sleep(2)
+            newer = len(driver.find_elements_by_class_name('retableRow'))
+            if newer == original:
+                break
+            else:
+                original = newer
+
         if show_output:
             print('Writing activity to \'netflix_activity.txt\'')
 
